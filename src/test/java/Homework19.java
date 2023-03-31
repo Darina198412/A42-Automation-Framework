9396e85d9b73676edeca8a6c651cc19fc63ca821
Darina1984 <daria.salina@testpro.io>
2023-03-31 19:40:51 -0400

    delete playlist-3

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Homework19 extends BaseTest {
-
-
-
+    @Test
+    public void deleteSong() {
+        //login
+    }
 }
